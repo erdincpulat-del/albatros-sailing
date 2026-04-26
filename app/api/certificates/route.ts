c06b8e98779ea0c6c4027a7df6b081c1cf12ea3c
Erdinc Pulat <erdincpulat@gmail.com>
2026-04-27 02:52:20 +0300

    QR system added

--- app/api/certificates/route.ts
+++ app/api/certificates/route.ts
@@ -1,50 +1,58 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import crypto from "crypto";
+import QRCode from "qrcode";
 
 // ✅ 1. GET (zaten var)
 export async function POST(req: Request) {
   try {
-    const body = await req.json();
+    
+   const body = await req.json();
 
-    const {
-      fullName,
-      program,
-      qualificationLevel,
-      issueDate,
-      seaMiles,
-      instructorId,
-      photoUrl,
-    } = body;
+const {
+  fullName,
+  program,
+  qualificationLevel,
+  issueDate,
+  seaMiles,
+  instructorId,
+  photoUrl,
+} = body;
 
-    if (!fullName || !program || !qualificationLevel || !instructorId) {
-      return NextResponse.json(
-        { success: false, error: "Eksik alan var" },
-        { status: 400 }
-      );
-    }
+if (!fullName || !program || !qualificationLevel || !instructorId) {
+  return NextResponse.json(
+    { success: false, error: "Eksik alan var" },
+    { status: 400 }
+  );
+}
 
-    const count = await prisma.certificate.count();
+const count = await prisma.certificate.count();
 
-    const certificateId = `AS-OFF-2026-${String(count + 1).padStart(4, "0")}`;
+const certificateId = `AS-OFF-2026-${String(count + 1).padStart(4, "0")}`;
 
-    const verificationHash = crypto
-      .createHash("sha256")
-      .update(`${certificateId}-${Date.now()}`)
-      .digest("hex");
+const verificationHash = crypto
+  .createHash("sha256")
+  .update(`${certificateId}-${Date.now()}`)
+  .digest("hex");
 
-    const certificate = await prisma.certificate.create({
+// 🔥 QR üretim
+const verifyUrl = `https://albatros-sailing.vercel.app/verify/${certificateId}`;
+const qrCodeUrl = await QRCode.toDataURL(verifyUrl);
+
+const certificate = await prisma.certificate.create({
   data: {
     certificateId,
     fullName,
     program,
     qualificationLevel,
-    issueDate: issueDate ? new Date(issueDate.split(".").reverse().join("-")): null,
+    issueDate: issueDate
+      ? new Date(issueDate.split(".").reverse().join("-"))
+      : null,
     seaMiles: seaMiles ? Number(seaMiles) : null,
     photoUrl: photoUrl || null,
     status: "ACTIVE",
     verificationHash,
-
+    qrCodeUrl, // 👈 EKLENDİ
     instructorId: instructorId || "erdinc-pulat",
   },
 });
