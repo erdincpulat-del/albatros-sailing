64dc33389a3ea6b3e48310eec9d5e991fcfc4fca
Erdinc Pulat <erdincpulat@gmail.com>
2026-04-26 21:06:43 +0300

    fix prisma export

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma?: PrismaClient;
 };
 
-export const prisma =
+const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: ["error"],
@@ -13,3 +13,6 @@ export const prisma =
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
+
+export default prisma;
+export { prisma };
