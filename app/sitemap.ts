a214a13e3cf7b40adc8fdea9f0fd2ff4dac759ef
Erdinc Pulat <erdincpulat@gmail.com>
2026-05-17 01:43:27 +0300

    fix sitemap xml

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,29 +1,75 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://albatrossailing.com";
+  return [
+    {
+      url: "https://albatros-sailing.com.tr",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
 
-  const routes = [
-    "",
-    "/programs",
-    "/charter",
-    "/contact",
-    "/stories",
-    "/verify",
+    {
+      url: "https://albatros-sailing.com.tr/programs",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
 
-    "/yelkenli-yat-egitim-ve-tatil",
-    "/bodrum-yelken-egitimi",
-    "/offshore-yelken-egitimi",
-    "/konaklamali-yelken-egitimi",
-    "/yat-kaptanligi-egitimi",
-    "/yelkenli-yat-okulu",
-    "/ege-denizi-yelken-rotalari",
+    {
+      url: "https://albatros-sailing.com.tr/contact",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+
+    {
+      url: "https://albatros-sailing.com.tr/verify",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+
+    {
+      url: "https://albatros-sailing.com.tr/bodrum-yelken-egitimi",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+
+    {
+      url: "https://albatros-sailing.com.tr/offshore-yelken-egitimi",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+
+    {
+      url: "https://albatros-sailing.com.tr/konaklamali-yelken-egitimi",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+
+    {
+      url: "https://albatros-sailing.com.tr/yat-kaptanligi-egitimi",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+
+    {
+      url: "https://albatros-sailing.com.tr/yelkenli-yat-okulu",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+
+    {
+      url: "https://albatros-sailing.com.tr/ege-denizi-yelken-rotalari",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
   ];
-
-  return routes.map((route) => ({
-    url: `${baseUrl}${route}`,
-    lastModified: new Date(),
-    changeFrequency: "weekly",
-    priority: route === "" ? 1 : 0.9,
-  }));
 }
