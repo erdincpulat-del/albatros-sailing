da35301321d9f94d216db34846930cbf0876d890
Erdinc Pulat <erdincpulat@gmail.com>
2026-05-17 02:05:13 +0300

    manual xml sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,46 +1,52 @@
-import { MetadataRoute } from "next";
+export async function GET() {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: "https://albatros-sailing.com.tr",
-      lastModified: new Date(),
+  <url>
+    <loc>https://albatros-sailing.com.tr</loc>
+  </url>
+
+  <url>
+    <loc>https://albatros-sailing.com.tr/programs</loc>
+  </url>
+
+  <url>
+    <loc>https://albatros-sailing.com.tr/contact</loc>
+  </url>
+
+  <url>
+    <loc>https://albatros-sailing.com.tr/verify</loc>
+  </url>
+
+  <url>
+    <loc>https://albatros-sailing.com.tr/bodrum-yelken-egitimi</loc>
+  </url>
+
+  <url>
+    <loc>https://albatros-sailing.com.tr/offshore-yelken-egitimi</loc>
+  </url>
+
+  <url>
+    <loc>https://albatros-sailing.com.tr/konaklamali-yelken-egitimi</loc>
+  </url>
+
+  <url>
+    <loc>https://albatros-sailing.com.tr/yat-kaptanligi-egitimi</loc>
+  </url>
+
+  <url>
+    <loc>https://albatros-sailing.com.tr/yelkenli-yat-okulu</loc>
+  </url>
+
+  <url>
+    <loc>https://albatros-sailing.com.tr/ege-denizi-yelken-rotalari</loc>
+  </url>
+
+</urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml",
     },
-    {
-      url: "https://albatros-sailing.com.tr/programs",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://albatros-sailing.com.tr/contact",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://albatros-sailing.com.tr/verify",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://albatros-sailing.com.tr/bodrum-yelken-egitimi",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://albatros-sailing.com.tr/offshore-yelken-egitimi",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://albatros-sailing.com.tr/konaklamali-yelken-egitimi",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://albatros-sailing.com.tr/yat-kaptanligi-egitimi",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://albatros-sailing.com.tr/yelkenli-yat-okulu",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://albatros-sailing.com.tr/ege-denizi-yelken-rotalari",
-      lastModified: new Date(),
-    },
-  ];
+  });
 }
